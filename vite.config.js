d693660b55693bf784c6867f00b102225804aecc
liuyahua <1251271810@qq.com>
2024-12-24 23:50:52 +0800

    1

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,8 @@ import vueJsx from "@vitejs/plugin-vue-jsx";
 
 // https://vite.dev/config/
 export default defineConfig({
+   // 部署的前缀，这里的匹配方式是 username.github.io/repository 的形式
+   base: 'https://lyahua.github.io/vue-base-components/',
   plugins: [
     vueJsx(),
     vue(),
